901a0f52524aeb659cf1db59411efa7d5e580290
ogatakatsuya <katsuya151225@icloud.com>
2025-05-29 23:56:46 +0900

    add: markdown extention

---
+++ src/engine.mjs
@@ -0,0 +1,11 @@
+import container from 'markdown-it-container';
+import attrs from 'markdown-it-attrs';
+import mark from 'markdown-it-mark'
+
+/** 
+ * @type {import('@marp-team/marp-cli').Config<typeof import('@marp-team/marpit').Marpit>["engine"]}
+ */
+export default ({ marp }) => marp
+  .use(mark)
+  .use(attrs)
+  .use(container, 'code');
